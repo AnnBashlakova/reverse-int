e7115f193b314e56bd89faf37ac3ffc2b7ceac16
Anna <anna47297@gmail.com>
2022-02-19 11:19:41 +0300

    feat:add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let absloute = Math.abs(n);
+    const newStr = String(absloute);
+    let massive = newStr.split('');
+    let reverMas = massive.reverse();
+    
+    let word = +reverMas.join('');
+    // let number = +word;
+    return word;
 }
+
+
+
